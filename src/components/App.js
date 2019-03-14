90bfeb8e7b7442ea917cb0eb4b1ae67d8f9eb989
Tanner Scadden <tanner@scaddenfamily.com>
2019-03-14 15:33:41 -0600

    finished all projects and black diamond

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Axios from 'axios';
+import Post from './Post/Post'
+
+const baseUrl = 'https://practiceapi.devmountain.com/api/posts'
 
 class App extends Component {
   constructor() {
@@ -19,32 +23,62 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    Axios.get(baseUrl).then(res => {
+      this.setState({posts: res.data});
+    }).catch(() => console.log("Failed to mount component"));
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    Axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text }).then(res => {
+      this.setState({posts: res.data})
+    }).catch(() => console.log("Failed to update post"))
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    Axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`).then(res => {
+      this.setState({posts: res.data})
+    }).catch(() => console.log("Failed to delete post"))
   }
 
-  createPost() {
+  createPost(text) {
+    Axios.post(`https://practiceapi.devmountain.com/api/posts`, {text}).then(res => {
+      this.setState({posts: res.data})
+    }).catch(() => console.log("Failed to create post"))
+  }
 
+  filterResults = (word) => {
+    if (word) {
+      console.log(this.state.posts);
+      let filtered = this.state.posts.filter(post => encodeURI(post.text).includes(encodeURI(word)));
+      console.log(filtered)
+     this.setState({posts: filtered})
+    } else {
+      Axios.get(baseUrl).then(res => {
+        this.setState({posts: res.data});
+      }).catch(() => console.log("Failed to mount component"));
+    }
   }
 
   render() {
     const { posts } = this.state;
+    console.log(this.state.posts);
 
     return (
       <div className="App__parent">
-        <Header />
+        <Header filterResultsFn={this.filterResults}/>
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost}/>
+
+          {posts.map(post => (
+            <Post key={post.id}
+                  text={post.text}
+                  date={post.date}
+                  id={post.id}
+                  updatePostFn={this.updatePost}
+                  deletePostFn={this.deletePost}/>))}
+
         </section>
       </div>
     );
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,14 +7,33 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchWords: '',
+    }
+  }
+
+  search = (e) => {
+    this.setState({searchWords: e});
+  }
+
+  filterResults = () => {
+    const {filterResultsFn} = this.props;
+
+    filterResultsFn(this.state.searchWords);
+    this.setState({searchWords: ''})
+  }
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange={(e) => this.search(e.target.value)} />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon id="Search__icon" onClick={() => this.filterResults()} />
         </div>
         
       </section>
